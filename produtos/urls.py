107bed0c70003a23a71f3eaca072c37997d47e64
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-05-26 16:15:44 -0300

    feat(static): adicionado arquivos staticos.

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -9,4 +13,4 @@ urlpatterns = [
     path('form-categorias', views.create_categorias, name='create_categorias'),
     path('form-categorias', views.update_categorias, name='update_categorias'),   
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,8 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
+#python manage.py collectstatic
 
 python manage.py runserver 0.0.0.0:8000
