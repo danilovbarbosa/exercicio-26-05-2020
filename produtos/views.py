388571164b362fd08278627eee7a0083cbb1ef58
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-05-26 15:13:04 -0300

    feat(views): definida view categoria, com template e url.

--- produtos/views.py
+++ produtos/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'index.html', context=context)
+
+def categorias(request):
+    context = {}
+    return render(request, 'categorias.html', context=context)
