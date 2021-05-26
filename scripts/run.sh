17d02e25d9d3c1e872c7544f878a0a36bbdee645
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-05-26 13:32:41 -0300

    refactor(run): criação do superuser, com uma solução alternativa.

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,6 @@ python manage.py makemigrations
 
 python manage.py migrate
 
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
 python manage.py runserver 0.0.0.0:8000
